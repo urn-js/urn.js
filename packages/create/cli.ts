86d66792edcd4def0ffd2878b78127a8c1368021
maverickquill55 <consorceo300@icloud.com>
2023-06-22 00:32:52 -0400

    use fse, prompt, and ora

--- packages/create/cli.ts
+++ packages/create/cli.ts
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 import prompt from 'prompt';
-import { copy } from 'fs-extra/esm.mjs';
-import { oraPromise } from 'ora';
-console.log(process.env.NODE_DIR)
+import { copy } from 'fs-extra';
+const pkg = await import('ora')
+const { oraPromise } = pkg
+console.log(import.meta)
 async function clone() {
-
+    await oraPromise(async (spinner) => {
+        spinner.start("Cloning server source.")
+        await copy('./server', '/dev/null')
+        spinner.succeed()
+    }, {
+        spinner: 'arc',
+        color: 'cyan',
+        indent: 4
+    })
 }

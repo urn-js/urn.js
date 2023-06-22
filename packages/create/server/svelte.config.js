6a21981463545978350d3328319655a038e8989f
maverickquill55 <consorceo300@icloud.com>
2023-06-21 22:53:12 -0400

    add changes

---
+++ packages/create/server/svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess({
+		style: {
+			css: {
+				postcss: (await import('./postcss.config.cjs')).default
+			}
+		}
+	}),
+
+	kit: {
+		adapter: adapter({
+			split: true
+		})
+	}
+};
+
+export default config;
